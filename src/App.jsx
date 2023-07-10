2e81a094dd2f850b957e8a25f51bf0dabbdd9596
Yevhen <onufrii.socnet@gmail.com>
2023-07-10 18:41:58 +0300

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import './App.scss'
+import Container from './Container'
+import Logo from './Logo'
+import Wrapper from './Wrapper'
 
 function App() {
-  return <div className="">Hello world</div>
+  return (
+    <div className="app">
+      <Wrapper>
+        <Container>
+          <Logo />
+        </Container>
+      </Wrapper>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Logo.jsx
@@ -0,0 +1,12 @@
+export default function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo_img">
+        <img src="../public/rocket.svg" alt="logo todo list" />
+      </div>
+      <h1 className="title">
+        <span>to</span>do
+      </h1>
+    </div>
+  )
+}
